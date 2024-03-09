63f20e6f7376745a9c863ac224fd32c794aaf7e9
krongdew <51514863+krongdew@users.noreply.github.com>
2024-03-09 11:03:04 +0700

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "../App.css";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      <video src="/videos/spa1.mp4" autoPlay loop muted />
+      <video src="./videos/spa1.mp4" autoPlay loop muted />
       <h1>RELAXATION AWAITS</h1>
       <p>What are you wating for?</p>
       <div className="hero-btns">
